26dc82898bde20a3a086c7f9c9ffa61aa06f480d
Fairuz Hashim <fihashim@gmail.com>
2023-11-27 16:53:06 +0100

    api predicts correctly

--- image-captionig/api/fast.py
+++ image-captionig/api/fast.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from taxifare.ml_logic.preprocessor import preprocess_features
-# from taxifare.ml_logic.data import clean_data
-# from taxifare.ml_logic.model import initialize_model, compile_model, evaluate_model
-# from taxifare.ml_logic.registry import save_results, load_model
+from transformers import AutoProcessor, AutoModelForCausalLM
+import requests
+from PIL import Image
 app = FastAPI()
-# app.state.model = load_model()
+app.state.model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
+
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -17,31 +17,22 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-# @app.get("/predict")
-# def predict(
-        # pickup_datetime: str,  # 2013-07-06 17:18:00
-        # pickup_longitude: float,    # -73.950655
-        # pickup_latitude: float,     # 40.783282
-        # dropoff_longitude: float,   # -73.984365
-        # dropoff_latitude: float,    # 40.769802
-        # passenger_count: int
-    # ):      # 1
-    # """
-    # Make a single course prediction.
-    # Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
-    # Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
-    # """
+@app.get("/predict")
+def predict(url):
+    image = Image.open(requests.get(url=url, stream=True).raw)
+    app.state.model  = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
 
-    # store X_pred into a dataframe
-    # X_pred = pd.DataFrame(locals(), index=[0])
-    # X_pred['pickup_datetime'] = pd.Timestamp(pickup_datetime, tz="US/Eastern")
+    # preproces the image
+    processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
 
-    # # process X
-    # X_pred = preprocess_features(X_pred)
+    # converting image pixels to ids
+    generated_ids = app.state.model.generate(pixel_values=pixel_values, max_length=50)
 
-    # # predict y
-    # y_pred = app.state.model.predict(X_pred)
-    # return dict(fare_amount=float(y_pred))
+    # generates final caption of image
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    return str(generated_caption)
 
 @app.get("/")
 def root():
