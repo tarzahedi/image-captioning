c65fb6e352cc1952613b8cec6fce88ea61d45ddd
Fairuz Hashim <fihashim@gmail.com>
2023-11-29 10:03:02 +0100

    docker image build still failing

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='image_captioning',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/image-captioning-run'],
+      scripts=['scripts/image_captioning-run'],
       zip_safe=False)
