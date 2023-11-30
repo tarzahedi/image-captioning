9cf161e678c4d132399e62d92787265ebc4f91e9
Alice Schroeder <alice.sofie.schroeder@gmail.com>
2023-11-30 14:40:19 +0100

    Ask your Image and color tests

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ st.title("Image Captioning")
 
 # Streamlit app content
 st.header("Lets caption some pictures!")
+#st.write("This is :blue[test]")
+
+# Choice selector for the user
+choice = st.radio("Choose an option:", ("Upload Image", "Provide URL", "Take a New Image"))
 
 # File uploader widget
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -27,38 +31,45 @@ if uploaded_file is not None:
     #Process the uploaded image using PIL
     caption = predict_upload(img_file_buffer=uploaded_file)
 
+   # Process the uploaded image using PIL
     #image = Image.open(uploaded_file)
 
     # API endpoint for your FastAPI app
     #api_endpoint = "http://your-fastapi-app-endpoint/predict"  # Replace with your actual FastAPI endpoint
 
     # Make an API request to get the image caption
-    #response = requests.post(api_endpoint, files={"image": uploaded_file})
-    #if response.status_code == 200:
-        #caption = response.json().get("caption", "Caption not available")
+    # response = requests.get(api_endpoint, files={"image": uploaded_file})
+    # if response.status_code == 200:
+    #     caption = response.json().get("caption", "Caption not available")
 
-       #  Display the generated caption in the Streamlit UI
-    st.write("Image Caption:", caption)
+    #     # Display the generated caption in the Streamlit UI
+    #     st.write("Image Caption:", caption)
 
-        # Text-to-speech - reading out the caption
-    sound_file = BytesIO()
-    tts = gTTS(caption, lang='en')
-    tts.write_to_fp(sound_file)
-    st.audio(sound_file, format="audio/mp3", start_time=0)
+    #     # Text-to-speech - reading out the caption
+    #     sound_file = BytesIO()
+    #     tts = gTTS(caption, lang='en')
+    #     tts.write_to_fp(sound_file)
+    #     st.audio(sound_file, format="audio/mp3", start_time=0)
+
+    #     # Subheader for asking questions
+    #     st.subheader("Ask your image a question:")
+
+    #     # Ask a question input widget
+    #     question = st.text_input("")
+
+    #     # Check if a question is asked
+    #     if question:
+    #         # Assume another API endpoint for image question answering
+    #         qa_api_endpoint = "http://your-question-answering-api-endpoint/answer"  # Replace with your actual QA API endpoint
+
+    #         # Make an API request to get the answer
+    #         qa_response = requests.get(qa_api_endpoint, json={"image": uploaded_file, "question": question})
+    #         if qa_response.status_code == 200:
+    #             answer = qa_response.json().get("answer", "Answer not available")
+
+    #             # Display the answer in the Streamlit UI
+    #             st.write("Answer:", answer)
+    #         else:
+    #             st.error("Error processing the question. Please try again.")
 else:
     st.error("Error processing the image. Please try again.")
-
-
-
-
-    # Placeholder for the generated caption (replace with your actual logic)
-    #hardcoded_caption = "Ragdoll kitten sitting on a couch"
-
-    # Display the generated caption in the Streamlit UI
-    #st.write("Image Caption:", hardcoded_caption)
-
-    # Text-to-speech - reading out the caption
-    #sound_file = BytesIO()
-    #tts = gTTS(hardcoded_caption, lang='en')
-    #tts.write_to_fp(sound_file)
-    #st.audio(sound_file, format="audio/mp3", start_time=0)
