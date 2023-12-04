f97663fee84ff0f694ca6f55b1fe13ef0e84cf93
Alice Schroeder <alice.sofie.schroeder@gmail.com>
2023-12-04 11:42:53 +0100

    Added buttons, text to speech and some design changes

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import streamlit as st
 from PIL import Image
 import requests
 #import time
-# from gtts import gTTS
-# from io import BytesIO
+from gtts import gTTS
+from io import BytesIO
 #from image_interface.interface.main import open_image,preprocess,generate_caption
 # from fastapi import FastAPI, File, UploadFile
 # from typing import Annotated
 # from image_interface.api.fast import predict_caption, predict_upload
+#from elevenlabs import generate, play
+#from pydantic import BaseModel, ConfigDict
+
+
 base_url = "http://127.0.0.1:8000/"
 # Streamlit app title
 st.title("Image Captioning")
@@ -18,13 +22,17 @@ st.title("Image Captioning")
 st.header("Lets caption some pictures!")
 #st.write("This is :blue[test]")
 
-# Choice selector for the user
-choice = st.radio("Choose an option:", ("Upload Image", "Provide URL", "Take a New Image"))
+# Add a sidebar to the app
+with st.sidebar:
+    # Choice selector for the user in the sidebar
+    st.markdown("<p style='font-size:20px; font-family:sans-serif; font-weight: bold;'>Choose an option:</p>", unsafe_allow_html=True)
+    choice = st.radio("", ("Upload Image", "Provide URL", "Take a New Image"))
+
 
 if choice == "Provide URL":
 
 # # URL:
-    input_url = st.text_input('put url')
+    input_url = st.text_input('Upload image via url:')
 
     if input_url is not None:
             # image = Image.open(requests.get(input_url, stream=True).raw).convert("RGB")
@@ -38,26 +46,35 @@ if choice == "Provide URL":
             #time.sleep (4)
 
         if response.status_code == 200:
-            image = Image.open(requests.get(input_url, stream=True).raw).convert("RGB")
-            st.image(image)
-            st.write("Caption: ", response.json())
-            question = st.text_input("question?")
+            caption = response.json()
+            st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
+# Text-to-speech - reading out the caption
+            sound_file = BytesIO()
+            tts = gTTS(caption, lang='en')
+            tts.write_to_fp(sound_file)
+            st.audio(sound_file, format="audio/mp3", start_time=0)
+
+# ASK YOUR IMAGE
+            st.subheader("Ask your image a question:")
 
 
-            if question is not None:
+# Get the question from the user
+            question = st.text_input("Your question:")
+
+
+            if st.button("Get Answer") and question:
                 params = {"url": input_url, "question": question}
                 api_endpoint = f"{base_url}url_answer"
                 res = requests.get(api_endpoint, params=params)
                 if res.status_code == 200:
-                    st.write("Answer: ", res.json())
-            else:
-                st.wtire("say")
+                    st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
+
 
 
 elif choice == "Upload Image":
 
 # # Upload_file:
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"]) #Upload photo
+    uploaded_file = st.file_uploader("Upload an image:", type=["jpg", "jpeg", "png"]) #Upload photo
 
 
     if uploaded_file is not None:
@@ -67,30 +84,35 @@ elif choice == "Upload Image":
         files = {'file': img}
         response = requests.post(api_endpoint, files=files)
         if response.status_code == 200:
-            st.write("Caption: ", response.json())
+            caption = response.json()
+            st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
+# Text-to-speech - reading out the caption
+            sound_file = BytesIO()
+            tts = gTTS(caption, lang='en')
+            tts.write_to_fp(sound_file)
+            st.audio(sound_file, format="audio/mp3", start_time=0)
 
-            question = st.text_input("question?")
+# ASK YOUR IMAGE
+            st.subheader("Ask your image a question:")
+
+# Get the question from the user
+            question = st.text_input("Your question:")
 
 
-            if question is not None:
+            if st.button("Get Answer") and question:
                 files = {"file": img}
                 params = {"question":question}
                 api_endpoint = f"{base_url}visual_q"
                 res = requests.post(api_endpoint, files=files, params=params)
                 if res.status_code == 200:
-                    st.write("Answer: ", res.json())
-            else:
-                st.wtire("say")
-
-
-
+                    st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
 
 
 
 elif choice == "Take a New Image":
 
 # # Take photo:
-    captured_photo = st.camera_input("Choose an image...") #Take photo
+    captured_photo = st.camera_input("Take a photo:") #Take photo
 
     if captured_photo is not None:
         api_endpoint = f"{base_url}predict_image"
@@ -98,76 +120,29 @@ elif choice == "Take a New Image":
         files = {'file': img}
         response = requests.post(api_endpoint, files=files)
         if response.status_code == 200:
-            st.write("Caption: ", response.json())
+            caption = response.json()
+            st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
+# Text-to-speech - reading out the caption
+            sound_file = BytesIO()
+            tts = gTTS(caption, lang='en')
+            tts.write_to_fp(sound_file)
+            st.audio(sound_file, format="audio/mp3", start_time=0)
 
-            question = st.text_input("question?")
+# ASK YOUR IMAGE
+            st.subheader("Ask your image a question:")
 
-            if question is not None:
+# Get the question from the user
+            question = st.text_input("Your question:")
+
+            if st.button("Get Answer") and question:
                 files = {"file": img}
                 params = {"question":question}
                 api_endpoint = f"{base_url}visual_q"
                 res = requests.post(api_endpoint, files=files, params=params)
                 if res.status_code == 200:
-                    st.write("Answer: ", res.json())
-            else:
-                st.wtire("say")
+                    st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
+
 
 
 else:
     st.write("Choose an option")
-
-
-
-# File uploader widget
-# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-# # Check if a file is uploaded
-# if uploaded_file is not None:
-#     # Display the uploaded image
-#     st.image(uploaded_file, use_column_width=True)
-
-#     #Process the uploaded image using PIL
-#     caption = predict_upload(img_file_buffer=uploaded_file)
-
-#    # Process the uploaded image using PIL
-#     #image = Image.open(uploaded_file)
-
-#     # API endpoint for your FastAPI app
-#     #api_endpoint = "http://your-fastapi-app-endpoint/predict"  # Replace with your actual FastAPI endpoint
-
-#     # Make an API request to get the image caption
-#     # response = requests.get(api_endpoint, files={"image": uploaded_file})
-#     # if response.status_code == 200:
-#     #     caption = response.json().get("caption", "Caption not available")
-
-#     #     # Display the generated caption in the Streamlit UI
-#     #     st.write("Image Caption:", caption)
-
-#     #     # Text-to-speech - reading out the caption
-#     #     sound_file = BytesIO()
-#     #     tts = gTTS(caption, lang='en')
-#     #     tts.write_to_fp(sound_file)
-#     #     st.audio(sound_file, format="audio/mp3", start_time=0)
-
-#     #     # Subheader for asking questions
-#     #     st.subheader("Ask your image a question:")
-
-#     #     # Ask a question input widget
-#     #     question = st.text_input("")
-
-#     #     # Check if a question is asked
-#     #     if question:
-#     #         # Assume another API endpoint for image question answering
-#     #         qa_api_endpoint = "http://your-question-answering-api-endpoint/answer"  # Replace with your actual QA API endpoint
-
-#     #         # Make an API request to get the answer
-#     #         qa_response = requests.get(qa_api_endpoint, json={"image": uploaded_file, "question": question})
-#     #         if qa_response.status_code == 200:
-#     #             answer = qa_response.json().get("answer", "Answer not available")
-
-#     #             # Display the answer in the Streamlit UI
-#     #             st.write("Answer:", answer)
-#     #         else:
-#     #             st.error("Error processing the question. Please try again.")
-# else:
-#     st.error("Error processing the image. Please try again.")
