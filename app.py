0d94583ded973b017e9d5798ff183d5891f28fb7
Taravat Zahedi <55549574+tarzahedi@users.noreply.github.com>
2023-12-06 16:17:03 +0100

    Old version for using in streamlit cloud with api

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from PIL import Image
 import requests
 from streamlit_option_menu import option_menu
 
-
 base_url = "http://127.0.0.1:8000/"
-#base_url = "https://imagecap-siqf5cui7q-ew.a.run.app/"
+# base_url = "https://imagecap-siqf5cui7q-ew.a.run.app/"
 
 # Streamlit app title
-st.title(":camera_with_flash: :speech_balloon: Let's Caption Your Image")
+st.title(":camera_with_flash: :speech_balloon: :camera_with_flash: :speech_balloon: :camera_with_flash: :speech_balloon: :camera_with_flash: :speech_balloon: :camera_with_flash: :speech_balloon: :camera_with_flash:")
+
+# Streamlit app content
+#st.header("Lets caption some pictures!")
 
 # Add a sidebar to the app
 with st.sidebar:
@@ -20,8 +22,15 @@ with st.sidebar:
         selected = option_menu("Main Menu", ["Home","Upload Image", 'Provide URL', "Take a New Image"],
         icons=['house', 'gear','eye','camera'], default_index=1)
         selected
+    #st.markdown("<p style='font-size:20px; font-family:sans-serif; font-weight: bold;color: #523F6D;'>Choose an option:</p>", unsafe_allow_html=True)
+   # choice = st.radio("", ("Upload Image", "Provide URL", "Take a New Image"))
 
-## HOME PAGE
+# # Initialize session state
+# if 'show_uploader' not in ss:
+#     ss['show_uploader'] = True
+
+# if 'image' not in ss:
+#     ss['image'] = None
 
 if selected == "Home":
     st.header("Caption your pictures")
@@ -31,22 +40,25 @@ if selected == "Home":
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2ew7u6dEGWAZWQJcjZ5yofPZuWdwuKJb2HRch22JR8yyIlSLDW29nHzWMFuw2pG01X-A&usqp=CAU")
-        st.write("Caption: dogs on the beach")
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+
     with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
 
-        st.image("https://media-cldnry.s-nbcnews.com/image/upload/newscms/2017_03/1868126/ss-170117-men-who-walked-moon-duke-mn-11.jpg")
-        st.write("Caption: astronaut on the moon during space mission")
     with col3:
-
-        st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_mVdZJqudtSUtFyZfoUUZj74P_Y8Lv3GKlQ&usqp=CAU")
-        st.write("Caption: football player celebrates after scoring a goal during the final against football team")
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
 
     st.subheader("And ask your image questions")
     st.write("Our AI tool transforms the passive act of viewing into an interactive exploration, allowing you to inquire about the details that matter most to you. Start asking it directly!")
 
-## UPLOAD URL
+    st.subheader("Try it out now and choose an option on the left")
+
+# Main content
 if selected == "Provide URL":
+    # # URL:
     input_url = st.text_input('Upload image via url:')
 
     if input_url is not None:
@@ -58,14 +70,15 @@ if selected == "Provide URL":
             st.image(image)
             caption = response.json()
             st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
-#TEXT TO SPEECH - Caption
+#TEXT TO SPEECH
             sound_file = BytesIO()
             tts = gTTS(caption, lang='en')
             tts.write_to_fp(sound_file)
             st.audio(sound_file, format="audio/mp3", start_time=0)
 
 #ASK YOUR IMAGE
-            st.title(":camera_with_flash: :grey_question: Now Ask It a Question:")
+            st.title(":camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash: 	:grey_question: :camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash:")
+            st.header("Now ask your image a question!")
             question = st.text_input("Your question:")
 
             if st.button("Get Answer") and question:
@@ -73,15 +86,11 @@ if selected == "Provide URL":
                 api_endpoint = f"{base_url}url_answer"
                 res = requests.get(api_endpoint, params=params)
                 if res.status_code == 200:
-                    answer = res.json()
                     st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
-#TEXT TO SPEECH - Answer
-                    sound_file = BytesIO()
-                    tts = gTTS(answer, lang='en')
-                    tts.write_to_fp(sound_file)
-                    st.audio(sound_file, format="audio/mp3", start_time=0)
-## UPLOAD IMAGE
+
 elif selected == "Upload Image":
+    # # Upload_file:
+   # if ss['show_uploader']:
     uploaded_file = st.file_uploader("Upload an image:", type=["jpg", "jpeg", "png"])  # Upload photo
 
     if uploaded_file is not None:
@@ -94,14 +103,29 @@ elif selected == "Upload Image":
             caption = response.json()
             st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(caption), unsafe_allow_html=True)
 
-#TEXT TO SPEECH - Caption
+        # if uploaded_file is not None:
+        #     ss['image'] = uploaded_file  # backup the file
+        #     ss['show_uploader'] = False
+        #     st.rerun()
+
+    # if ss['image'] is not None:
+    #     st.image(ss['image'], use_column_width=True)
+    #     api_endpoint = f"{base_url}predict_image"
+    #     img = ss['image'].getvalue()
+    #     files = {'file': img}
+    #     response = requests.post(api_endpoint, files=files)
+    #     if response.status_code == 200:
+    #         caption = response.json()
+    #         st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
+#TEXT TO SPEECH
             sound_file = BytesIO()
             tts = gTTS(caption, lang='en')
             tts.write_to_fp(sound_file)
             st.audio(sound_file, format="audio/mp3", start_time=0)
 
 #ASK YOUR IMAGE
-            st.title(":camera_with_flash: :grey_question: Now Ask It a Question:")
+            st.title(":camera_with_flash: :grey_question: :camera_with_flash: 	:grey_question: :camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash:")
+            st.header("Now ask your image a question!")
             question = st.text_input("Your question:")
 
             if st.button("Get Answer") and question:
@@ -110,16 +134,10 @@ elif selected == "Upload Image":
                 api_endpoint = f"{base_url}visual_q"
                 res = requests.post(api_endpoint, files=files, params=params)
                 if res.status_code == 200:
-                    answer = res.json()
                     st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
-#TEXT TO SPEECH - Answer
-                    sound_file = BytesIO()
-                    tts = gTTS(answer, lang='en')
-                    tts.write_to_fp(sound_file)
-                    st.audio(sound_file, format="audio/mp3", start_time=0)
 
-## TAKE A NEW IMAGE
 elif selected == "Take a New Image":
+    # # Take photo:
     captured_photo = st.camera_input("Take a photo:")  # Take photo
 
     if captured_photo is not None:
@@ -130,14 +148,15 @@ elif selected == "Take a New Image":
         if response.status_code == 200:
             caption = response.json()
             st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Caption: {}</p>".format(response.json()), unsafe_allow_html=True)
-#TEXT TO SPEECH - Caption
+#TEXT TO SPEECH
             sound_file = BytesIO()
             tts = gTTS(caption, lang='en')
             tts.write_to_fp(sound_file)
             st.audio(sound_file, format="audio/mp3", start_time=0)
 
-#ASK YOUR IMAGE
-            st.title(":camera_with_flash: :grey_question: Now Ask It a Question:")
+ #ASK YOUR IMAGE
+            st.title(":camera_with_flash: :grey_question: :camera_with_flash: 	:grey_question: :camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash: :grey_question: :camera_with_flash:")
+            st.header("Now ask your image a question!")
             question = st.text_input("Your question:")
 
             if st.button("Get Answer") and question:
@@ -146,10 +165,4 @@ elif selected == "Take a New Image":
                 api_endpoint = f"{base_url}visual_q"
                 res = requests.post(api_endpoint, files=files, params=params)
                 if res.status_code == 200:
-                    answer = res.json()
                     st.markdown("<p style='font-size:25px; font-family:sans-serif;'>Answer: {}</p>".format(res.json()), unsafe_allow_html=True)
-#TEXT TO SPEECH - Answer
-                    sound_file = BytesIO()
-                    tts = gTTS(answer, lang='en')
-                    tts.write_to_fp(sound_file)
-                    st.audio(sound_file, format="audio/mp3", start_time=0)
